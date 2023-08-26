29ac5408d6298c92216a5df3150e9a5c22ed1639
sharathsl <sharth.sl@gmail.com>
2023-08-26 12:20:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,89 @@
+provider "aws" {
+  region = var.region
+}
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  cluster_name = "petclinic-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = "petclinic-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.16.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.23"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    # two = {
+    #   name = "node-group-2"
+
+    #   instance_types = ["t3.small"]
+
+    #   min_size     = 1
+    #   max_size     = 2
+    #   desired_size = 1
+    # }
+  }
+}
+
+module "efs_csi_driver" {
+  source = "git::https://github.com/DNXLabs/terraform-aws-eks-efs-csi-driver.git"
+
+  cluster_name                     = module.eks_cluster.cluster_id
+  cluster_identity_oidc_issuer     = module.eks_cluster.cluster_oidc_issuer_url
+  cluster_identity_oidc_issuer_arn = module.eks_cluster.oidc_provider_arn
+}
